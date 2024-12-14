faed019ee5ce8d93c59192944bb303a5829e2476
Spirit丶守护 <71209933+wyq1229409654@users.noreply.github.com>
2024-12-15 03:01:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  "646f636b6572.boown.com": dockerHub,
-  "71756179.boown.com": "https://quay.io",
-  "676372.boown.com": "https://gcr.io",
-  "6b38732d676372.boown.com": "https://k8s.gcr.io",
-  "6b3873.boown.com": "https://registry.k8s.io",
-  "67686372.boown.com": "https://ghcr.io",
-  "636c6f7564736d697468.boown.com": "https://docker.cloudsmith.io",
-  "70797069.boown.com": "https://pypi.org",
-  "6e706d6a73.boown.com": "https://registry.npmjs.org",
-  "636e706d6a73.boown.com": "http://r.cnpmjs.org",
-};
+    "docker.shouhu.icu": "https://registry-1.docker.io",
+    "quay.shouhu.icu": "https://quay.io",
+    "gcr.shouhu.icu": "https://gcr.io",
+    "k8s-gcr.shouhu.icu": "https://k8s.gcr.io",
+    "k8s.shouhu.icu": "https://registry.k8s.io",
+    "ghcr.shouhu.icu": "https://ghcr.io",
+    "cloudsmith.shouhu.icu": "https://docker.cloudsmith.io",
+    "pypi.shouhu.icu": "https://pypi.org",
+    "npmjs.shouhu.icu": "https://registry.npmjs.org",
+    "cnpmjs.shouhu.icu": "http://r.cnpmjs.org",
+   };
 
 function routeByHosts(host) {
   if (host in routes) {
